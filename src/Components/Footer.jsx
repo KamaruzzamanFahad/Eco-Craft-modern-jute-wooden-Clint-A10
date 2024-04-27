6fdef7fbaf968a3d406333395cb01e8652672154
KamaruzzamanFahad <mihacker41@gmail.com>
2024-04-27 16:06:18 +0600

    Error 404 page creading done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,56 +4,76 @@ import { IoLocationSharp } from "react-icons/io5";
 
 const Footer = () => {
     return (
-        <footer className=" flex flex-col lg:flex-row gap-10 items-center  justify-between p-10 text-base-content bg-[#0000009e]">
-        <div>
-            <div className='flex items-center gap-2'>
-                <div className='bg-yellow-500 p-[5px] h-1 w-1'></div>
-                <p className='font-extrabold text-xl'>Our Partners</p>
-            </div>
-            <div className="mt-14">
-                <img src="payments.png" alt="" className="w-52" />
-            </div>
-        </div>
+        <div className="bg-[#0000009e]">
+            <footer className=" flex flex-col lg:flex-row gap-10 items-center  justify-between p-10 text-base-content ">
+                <div>
+                    <div className='flex items-center gap-2'>
+                        <div className='bg-yellow-500 p-[5px] h-1 w-1'></div>
+                        <p className='font-extrabold text-xl'>About Us</p>
+                    </div>
+                    <div>
+                        <h2 className="font-semibold text-xl mt-2 mb-1">Eco-Craft</h2>
+                        <p className="mr-[5%]">A Art & Craft Jute & wooden Crafts Website</p>
+                    </div>
 
-        <div className='ml-[-60px] w-[60%] sm:w-auto '>
-            <div className='flex items-center gap-2'>
-                <div className='bg-yellow-500 p-[5px] h-1 w-1'></div>
-                <p className='font-extrabold text-xl'>Contact Us
-                </p>
-            </div>
-            <div className='flex items-center gap-2 text-lg mt-5'>
-                <IoCall className='text-2xl' />
-                <p>1-600-500-6730</p>
-            </div>
-            <div className='flex items-center gap-2 text-lg mt-3'>
-                <MdOutlineEmail className='text-2xl' />
-                <p>info@Eco-Craft.org</p>
-            </div>
-            <div className='flex items-center gap-2 text-lg mt-3'>
-                <IoLocationSharp className='text-3xl' />
-                <p>856 Grand Ave, Eco <br></br>
-                    Uganda Way, FLCraft 565</p>
-            </div>
-        </div>
-
-        <form >
-            <div className='flex items-center gap-2'>
-                <div className='bg-yellow-500 p-[5px] h-1 w-1'></div>
-                <p className='font-extrabold text-xl'>Newsletter
-                </p>
-            </div>
-            <fieldset className="form-control ">
-                <label className="label mt-2">
-                    <span className="label-text">Enter your e-mail to get the latest news of MyHome</span>
-                </label>
-                <div className="join mt-2 bg-red-600 w-[95%]">
-                    <input type="text" placeholder="Your e-mail" className="input input-bordered join-item w-full" />
-                    <button disabled className="bg-blue-500 text-white rounded-l-none ml-[-2px]">Subscribe</button>
+                    <div className='flex items-center gap-2 mt-5'>
+                        <div className='bg-yellow-500 p-[5px] h-1 w-1'></div>
+                        <p className='font-extrabold text-xl'>Our Partners</p>
+                    </div>
+                    <div className="mt-5">
+                        <img src="payments.png" alt="" className="w-52" />
+                    </div>
                 </div>
-                <img src="socai.png" width={'30%'} alt="" className='mt-4' />
-            </fieldset>
-        </form>
-    </footer>
+
+                <div className='ml-[-60px] w-[60%] sm:w-auto '>
+                    <div className='flex items-center gap-2'>
+                        <div className='bg-yellow-500 p-[5px] h-1 w-1'></div>
+                        <p className='font-extrabold text-xl'>Contact Us
+                        </p>
+                    </div>
+                    <div className='flex items-center gap-2 text-lg mt-5'>
+                        <IoCall className='text-2xl' />
+                        <p>1-600-500-6730</p>
+                    </div>
+                    <div className='flex items-center gap-2 text-lg mt-3'>
+                        <MdOutlineEmail className='text-2xl' />
+                        <p>info@Eco-Craft.org</p>
+                    </div>
+                    <div className='flex items-center gap-2 text-lg mt-3'>
+                        <IoLocationSharp className='text-3xl' />
+                        <p>856 Grand Ave, Eco <br></br>
+                            Uganda Way, FLCraft 565</p>
+                    </div>
+                </div>
+
+                <form >
+                    <div className='flex items-center gap-2'>
+                        <div className='bg-yellow-500 p-[5px] h-1 w-1'></div>
+                        <p className='font-extrabold text-xl'>Newsletter
+                        </p>
+                    </div>
+                    <fieldset className="form-control ">
+                        <label className="label mt-2">
+                            <span className="label-text">Enter your e-mail to get the latest news of MyHome</span>
+                        </label>
+                        <div className="join mt-2 bg-red-600 w-[95%]">
+                            <input type="text" placeholder="Your e-mail" className="input input-bordered join-item w-full" />
+                            <button disabled className="bg-blue-500 text-white rounded-l-none ml-[-2px]">Subscribe</button>
+                        </div>
+                        <img src="socai.png" width={'30%'} alt="" className='mt-4' />
+                    </fieldset>
+
+                    <div className='flex items-center gap-2 mt-4'>
+                        <div className='bg-yellow-500 p-[5px] h-1 w-1'></div>
+                        <p className='font-extrabold text-xl'>Get in Tuch
+                        </p>
+                    </div>
+                    <img src="soca.png" alt="" className="w-36" />
+                </form>
+            </footer>
+
+            <h1 className="text-center text-lg text-[#d4d4d4] pb-5">Copyright Eco-Craft ! All Rights Reserved</h1>
+        </div>
     );
 };
 
--- src/Pages/MyArt.jsx
+++ src/Pages/MyArt.jsx
@@ -19,8 +19,45 @@ const MyArt = () => {
 
     return (
         <div>
-            my art
+        <div className='mt-14'>
+            <div>
+                <h1 className='text-center mb-5'>My Art & Craft List</h1>
+            </div>
+
+            <div className='grid gap-5 grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+                {
+                    data.map((item, i) => (
+                        <div key={i} className='bg-[#eeeeee] flex flex-col h-auto p-4 pb-6 rounded-lg animate__bounceIn'>
+                            <div>
+                                <img src={item.photo} width={'100%'} alt="" className="rounded-xl h-[200px]" />
+                            </div>
+                            <a className='text-sm'>{item.stockStatus}</a>
+                            <p className='text-blue-500 font-semibold text-lg py-1'>{item.name}</p>
+                            <div className='flex items-center gap-2'>
+                                <Rating
+                                    emptySymbol={<img src="/starempty.png" className="icon w-5" />}
+                                    fullSymbol={<img src="/starfull.png" className="icon w-5" />}
+                                    initialRating={item.rating}
+                                    fractions={5}
+                                    readonly
+                                />
+                                <p>{item.rating}</p>
+                            </div>
+
+                            <div>
+                                <p>Processing time: {item.processing_time}</p>
+                                <p>Customizable: {item.customizable}</p>
+                            </div>
+                            <p className='text-black h-full text-xl font-semibold mb-2'>{item.price}</p>
+                            <button className='w-full bg-blue-500 text-white'>Update</button>
+                            <button className='w-full bg-red-400 text-white mt-2'>Delete</button>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
+
+    </div>
     );
 };
 
